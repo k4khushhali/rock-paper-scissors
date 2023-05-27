38c8499b00e64a425cd7dd4bb4dcf975d42294a7
k4khushhali <k4khushhali@gmail.com>
2023-05-27 13:12:05 +0100

    Basic game layout

---
+++ README.md
@@ -0,0 +1,7 @@
+Broken Rock Paper Scissors
+--------------------------
+
+In this project create a three page web application to play rock, paper, scissors: 
+
+http://en.wikipedia.org/wiki/Rock-paper-scissors
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Khushhali Jani - Rock Paper Scissors</title>
+  <?php require_once "bootstrap.php"; ?>
+</head>
+
+<body>
+  <div class="container">
+    <h1>Welcome to Rock Paper Scissors</h1>
+
+    <p>
+      <a href="login.php" class="btn btn-primary">Please Log In</a>
+    </p>
+    <p>About game
+      <a href="https://en.wikipedia.org/wiki/Rock_paper_scissors">click here</a>.
+    </p>
+  </div>
+</body>
